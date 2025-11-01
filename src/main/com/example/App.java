b7c33117b2bacd1e7550c3be778cc53945133dd1
Mostafa Elelemy <mostelelemy@gmail.com>
2025-11-01 15:10:17 +0200

    push

---
+++ src/main/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello from Jenkins!");
+  }
+
+  public static int add(int a, int b) { return a + b; }
+}
---
+++ src/test/java/com/AppTest.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+  @Test
+  void adds() { assertEquals(4, App.add(2,2)); }
+}
